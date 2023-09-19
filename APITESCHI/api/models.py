a7253301e18bd4dabd86f86fbf1a12cd1d3a1f26
VictorPM444 <victorpmejia2701@gmail.com>
2023-09-19 12:42:54 -0600

    Creacion de primer tabla

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+#Aqui se crea el modelo de la BD (esqueleto)
+
+class Marca(models.Model):
+    idMarca = models.AutoField(primary_key=True,db_column='idGenero')
+    nombreMarca = models.TextField(db_column='nombreMarca')
+    class Meta:
+        db_table='Marcas'
+
