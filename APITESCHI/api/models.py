3ab0b2fad0d008fec5caf7789abc8fd2c7493be9
VictorPM444 <victorpmejia2701@gmail.com>
2023-09-19 12:51:33 -0600

    add

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -3,9 +3,18 @@ from django.db import models
 # Create your models here.
 #Aqui se crea el modelo de la BD (esqueleto)
 
+#########################  ENTIDADES  #####################
+
+#metodo para la creacion de la entidad marca
 class Marca(models.Model):
+    #creadion de ID autoincrementable y como llave primaria
     idMarca = models.AutoField(primary_key=True,db_column='idGenero')
-    nombreMarca = models.TextField(db_column='nombreMarca')
+    #creacion de atributos de entidad
+    nombreMarca = models.TextField(max_length=20,db_column='nombreMarca')
+    descripcion = models.TextField(max_length=150,db_column='descripcion')
+    #metodo de nombramiento de la entidad
     class Meta:
         db_table='Marcas'
+
+
 
