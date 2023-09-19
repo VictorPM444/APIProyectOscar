ce27301225c167646b5b73e3d3545a7022adddc1
VictorPM444 <victorpmejia2701@gmail.com>
2023-09-19 13:46:52 -0600

    add

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -15,14 +15,3 @@ class Marca(models.Model):
     #metodo de nombramiento de la entidad
     class Meta:
         db_table='Marcas'
-
-class Talla(models.Model):
-    idTalla = models.AutoField(primary_key=True,db_column='idTalla')
-    nombreTalla = models.IntegerField(db_column='nombreTalla')
-    class Meta:
-        db_table='Tallas'
-
-
-
-
-
