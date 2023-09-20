73adacd3f04365ddf6363d0016b1751cd11e71e2
VictorPM444 <victorpmejia2701@gmail.com>
2023-09-19 18:38:03 -0600

    dejar mi codigo original

--- APITESCHI/api/views.py
+++ APITESCHI/api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-#Hecho en clase para importar la APIView
 from rest_framework.views import APIView
-# Create your views here.
+import requests
 
-#Creacion de lista, con todo el index dentro
 class Home(APIView):
-    template_name="index.html"
-    def get(self,request):
-        return render(request,self.template_name)
+    template_name = "index.html"
+    
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    
