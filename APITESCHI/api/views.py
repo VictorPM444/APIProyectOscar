9f23e95ab52131142dacf79822ab39476e11cb7e
VictorPM444 <victorpmejia2701@gmail.com>
2023-09-20 05:00:09 -0600

    prueba correo

--- APITESCHI/api/views.py
+++ APITESCHI/api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+
+from django.core.mail import send_mail
+
 import requests
 
 class login(APIView):
@@ -8,5 +11,14 @@ class login(APIView):
 
     def get(self, request):
         return render(request, self.template_name)
+    
+#VISTA
+
+subject = 'Asunto del Correo'
+message = 'Este es el contenido del correo.'
+from_email = 'vipermxm@gmail.com'
+recipient_list = ['victorpmejia2701@gmail.com']
+
+send_mail(subject, message, from_email, recipient_list)
 
     
