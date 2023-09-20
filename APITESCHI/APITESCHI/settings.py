223b78adf58ca064f1491a7d166b7a376bafb2c9
VictorPM444 <victorpmejia2701@gmail.com>
2023-09-20 05:05:21 -0600

    add

--- APITESCHI/APITESCHI/settings.py
+++ APITESCHI/APITESCHI/settings.py
@@ -129,9 +129,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vipermxm@gmail.com'
-EMAIL_HOST_PASSWORD = 'AshuraDZoro444'
+
--- APITESCHI/api/views.py
+++ APITESCHI/api/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-
-from django.core.mail import send_mail
-
 import requests
 
 class login(APIView):
@@ -11,14 +8,4 @@ class login(APIView):
 
     def get(self, request):
         return render(request, self.template_name)
-    
-#VISTA
-
-subject = 'Asunto del Correo'
-message = 'Este es el contenido del correo.'
-from_email = 'vipermxm@gmail.com'
-recipient_list = ['victorpmejia2701@gmail.com']
-
-send_mail(subject, message, from_email, recipient_list)
-
     
