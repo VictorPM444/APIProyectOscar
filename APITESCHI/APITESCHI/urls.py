40b0e56eca2c58af5d606a20e1c4f8e13172e506
VictorPM444 <victorpmejia2701@gmail.com>
2023-09-26 08:54:57 -0600

    cambios a htmls necesarios hasta el momento

--- APITESCHI/APITESCHI/urls.py
+++ APITESCHI/APITESCHI/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 #se importa la vista de la API desde el Home que hicimos antes
-from api.views import login, home, shop, formulario,gallery,my_account
+from api.views import login, home, shop, formulario,gallery,my_account,shop_grid_left_sidebar,about_us,account_details,addressws,cart
+from api.views import coming_soon,compare,contact,downloads,faq,heading,index_3,index,modals,order_list,product_details,register
+from api.views import wishlist,shop_grid_no_sidebar,shop_grid_right_sidebar,shop_grid_top_sidebar,shop_list_left_sidebar,shop_list_no_sidebar
+from api.views import shop_list_right_sidebar,shop_list_top_sidebar
 
 
 urlpatterns = [
@@ -31,6 +34,31 @@ urlpatterns = [
    path('registro/',formulario.as_view(),name='registro'),   
    path('gallery/',gallery.as_view(),name='gallery'),
    path('my_account/',my_account.as_view(),name='my_account'),
+   path('shop_grid_left_sidebar/',shop_grid_left_sidebar.as_view(),name='shop_grid_left_sidebar'),
+   path('about_us/',about_us.as_view(),name='about_us'),
+   path('account_details/',account_details.as_view(),name='account_details'),
+   path('addressws/',addressws.as_view(),name='addressws'),
+   path('cart/',cart.as_view(),name='cart'),
+   path('coming_soon/',coming_soon.as_view(),name='coming_soon'),
+   path('compare/',compare.as_view(),name='compare'),
+   path('contact/',contact.as_view(),name='contact'),
+   path('downloads/',downloads.as_view(),name='downloads'),
+   path('faq/',faq.as_view(),name='faq'),
+   path('heading/',heading.as_view(),name='heading'),
+   path('index_3/',index_3.as_view(),name='index_3'),
+   path('index/',index.as_view(),name='index'),
+   path('modals/',modals.as_view(),name='modals'),
+   path('order_list/',order_list.as_view(),name='order_list'),
+   path('product_details/',product_details.as_view(),name='product_details'),
+   path('register/',register.as_view(),name='register'),
+   path('wishlist/',wishlist.as_view(),name='wishlist'),
+   path('shop_grid_no_sidebar/',shop_grid_no_sidebar.as_view(),name='shop_grid_no_sidebar'),
+   path('shop_grid_right_sidebar/',shop_grid_right_sidebar.as_view(),name='shop_grid_right_sidebar'),
+   path('shop_grid_top_sidebar/',shop_grid_top_sidebar.as_view(),name='shop_grid_top_sidebar'),
+   path('shop_list_left_sidebar/',shop_list_left_sidebar.as_view(),name='shop_list_left_sidebar'),
+   path('shop_list_no_sidebar/',shop_list_no_sidebar.as_view(),name='shop_list_no_sidebar'),
+   path('shop_list_right_sidebar/',shop_list_right_sidebar.as_view(),name='shop_list_right_sidebar'),
+   path('shop_list_top_sidebar/',shop_list_top_sidebar.as_view(),name='shop_list_top_sidebar'),
 
     #path('admin/',admin.site.urls)
 ]
--- APITESCHI/api/views.py
+++ APITESCHI/api/views.py
@@ -70,6 +70,158 @@ class my_account(APIView):
     def get(self, request):
         return render(request, self.template_name)
     
+class shop_grid_left_sidebar(APIView):
+    template_name= "shop_grid_left_sidebar.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class about_us(APIView):
+    template_name= "about_us.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class account_details(APIView):
+    template_name= "account_details.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class addressws(APIView):
+    template_name= "addressws.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class cart(APIView):
+    template_name= "cart.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class coming_soon(APIView):
+    template_name= "coming_soon.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class compare(APIView):
+    template_name= "compare.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class contact(APIView):
+    template_name= "contact.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class downloads(APIView):
+    template_name= "downloads.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class faq(APIView):
+    template_name= "faq.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class heading(APIView):
+    template_name= "heading.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class index_3(APIView):
+    template_name= "index_3.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class index(APIView):
+    template_name= "index.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class modals(APIView):
+    template_name= "modals.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class order_list(APIView):
+    template_name= "order_list.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class product_details(APIView):
+    template_name= "product_details.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class register(APIView):
+    template_name= "register.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class wishlist(APIView):
+    template_name= "wishlist.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+    
+class shop_grid_no_sidebar(APIView):
+    template_name= "shop_grid_no_sidebar.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class shop_grid_right_sidebar(APIView):
+    template_name= "shop_grid_right_sidebar.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class shop_grid_top_sidebar(APIView):
+    template_name= "shop_grid_top_sidebar.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class shop_list_left_sidebar(APIView):
+    template_name= "shop_list_left_sidebar.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class shop_list_no_sidebar(APIView):
+    template_name= "shop_list_no_sidebar.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class shop_list_right_sidebar(APIView):
+    template_name= "shop_list_right_sidebar.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class shop_list_top_sidebar(APIView):
+    template_name= "shop_list_top_sidebar.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
+
 
 
 
