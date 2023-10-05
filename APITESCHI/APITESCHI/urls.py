dfe09287ce67f4269c3039a9135ac12a2a991798
VictorPM444 <victorpmejia2701@gmail.com>
2023-10-04 20:16:21 -0600

    busqueda y excel

--- APITESCHI/APITESCHI/urls.py
+++ APITESCHI/APITESCHI/urls.py
@@ -13,34 +13,48 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-#se importa la vista de la API desde el Home que hicimos antes
-from api.views import login,home,my_account,about_us,account_details,addresses,cart,order_list,wishlist,shop,checkout_complate,checkout_1,checkout_2,checkout_4,checkout_5
+
+# se importa la vista de la API desde el Home que hicimos antes
+from api.views import (
+    login,
+    home,
+    my_account,
+    about_us,
+    account_details,
+    addresses,
+    cart,
+    order_list,
+    wishlist,
+    shop,
+    checkout_complate,
+    checkout_1,
+    checkout_2,
+    checkout_4,
+    checkout_5,
+)
 
 
 urlpatterns = [
-    #lo comentamos para que no se inicie con la ventana de django
-    #path('admin/', admin.site.urls),
-    
-    #se crea, el path de vista con el home creado nombrandolo login
-   
-   path('login/',login.as_view(),name='login'),
-   path('',home.as_view(),name='home'),   
-   path('my_account/',my_account.as_view(),name='my_account'),
-   path('about_us/',about_us.as_view(),name='about_us'),
-   path('account_details/',account_details.as_view(),name='account_details'),
-   path('addresses/',addresses.as_view(),name='addresses'),
-   path('cart/',cart.as_view(),name='cart'),
-   path('order_list/',order_list.as_view(),name='order_list'),
-   path('wishlist/',wishlist.as_view(),name='wishlist'),
-   path('shop/',shop.as_view(),name='shop'),
-   path('checkout_complate/',checkout_complate.as_view(),name='checkout_complate'),
-   path('checkout_1/',checkout_1.as_view(),name='checkout_1'),
-   path('checkout_2/',checkout_2.as_view(),name='checkout_2'),
-   path('checkout_4/',checkout_4.as_view(),name='checkout_4'),
-   path('checkout_5/',checkout_5.as_view(),name='checkout_5'),
-    #path('admin/',admin.site.urls)
+    # lo comentamos para que no se inicie con la ventana de django
+    # path('admin/', admin.site.urls),
+    # se crea, el path de vista con el home creado nombrandolo login
+    path("login/", login.as_view(), name="login"),
+    path("", home.as_view(), name="home"),
+    path("my_account/", my_account.as_view(), name="my_account"),
+    path("about_us/", about_us.as_view(), name="about_us"),
+    path("account_details/", account_details.as_view(), name="account_details"),
+    path("addresses/", addresses.as_view(), name="addresses"),
+    path("cart/", cart.as_view(), name="cart"),
+    path("order_list/", order_list.as_view(), name="order_list"),
+    path("wishlist/", wishlist.as_view(), name="wishlist"),
+    path("shop/", shop.as_view(), name="shop"),
+    path("checkout_complate/", checkout_complate.as_view(), name="checkout_complate"),
+    path("checkout_1/", checkout_1.as_view(), name="checkout_1"),
+    path("checkout_2/", checkout_2.as_view(), name="checkout_2"),
+    path("checkout_4/", checkout_4.as_view(), name="checkout_4"),
+    path("checkout_5/", checkout_5.as_view(), name="checkout_5"),
 
+    # path('admin/',admin.site.urls)
+    # Otras URL de la aplicación
 ]
-
--- APITESCHI/api/views.py
+++ APITESCHI/api/views.py
@@ -1,60 +1,83 @@
-#renderiza y redirige
-from django.shortcuts import render,redirect
+# renderiza y redirige
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
-#importo la bd de usuario
+
+# importo la bd de usuario
 from .models import Usuario
-#para los correos
+
+# para los correos
 from django.core.mail import send_mail
-
-
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
 
 class login(APIView):
-    #encapsulo el login para su llamado
+    # encapsulo el login para su llamado
     template_name = "login.html"
 
     def get(self, request):
-        return render(request, self.template_name)
-     
-    #metodo de envio de informacion para almacenar
+        
+        return render(request, self.template_name, {"error": ""})
+
+    # metodo de envio de informacion para almacenar
     def post(self, request):
         try:
+            if request.method == "POST":
+                email = request.POST["email"]
 
-            if request.POST["password1"] == request.POST["password2"]:
-                print("hola registro")
+                # Verifica si el correo ya existe en la base de datos
+                if Usuario.objects.filter(correoElectronico=email).exists():
+                    mensaje = "El correo electrónico ya está registrado. Por favor, usa otro correo."
+                    # return render(request, 'login.html', {'mensaje': mensaje})
+                    return render(request, self.template_name, {"error": mensaje})
+                else:
+                    if request.POST["password1"] == request.POST["password2"]:
+                        print("hola registro")
+                        user = Usuario(
+                            nombreUsuario=request.POST["nombre"],
+                            apellidoPaterno=request.POST["apellidoPaterno"],
+                            apellidoMaterno=request.POST["apellidoMaterno"],
+                            password=request.POST["password1"],
+                            correoElectronico=request.POST["email"],
+                            numeroTelefonico=request.POST["numeroTelefono"],
+                        )
+                        user.save()
 
-            else:
-                print("adios")
+                        # Luego, prepara y envía el correo electrónico
+                        subject = "Registro Exitoso"
+                        from_email = "vipermxm@gmail.com"
+                        recipient_list = [request.POST["email"]]
 
+                        # Utiliza la plantilla HTML para el correo electrónico
+                        html_message = render_to_string(
+                            "bienvenida.html",
+                            {
+                                "nombre": request.POST["nombre"]
+                                + " "
+                                + request.POST["apellidoPaterno"]
+                                + " "
+                                + request.POST["apellidoMaterno"]
+                            },
+                        )
+                        # {{nombre}} es como se llama la variable que mandamos
+                        # Envía el correo electrónico
+                        send_mail(
+                            subject,
+                            strip_tags(html_message),
+                            from_email,
+                            recipient_list,
+                            html_message=html_message,
+                        )
+                        return redirect("home")
 
-            user=Usuario(
-               
-                    nombreUsuario=request.POST["nombre"],
-                    apellidoPaterno=request.POST["apellidoPaterno"],
-                    apellidoMaterno=request.POST["apellidoMaterno"],
-                    password=request.POST["password1"],
-                    correoElectronico=request.POST["email"],
-                    numeroTelefonico=request.POST["numeroTelefono"]
-            )
-            user.save()
+                    else:
+                        contra_diff = "La contraseña no es la misma. Por favor, reintente de nuevo."
+                        return render(
+                            request, self.template_name, {"error": contra_diff}
+                        )
 
-            # Luego, prepara y envía el correo electrónico
-            subject = 'Registro Exitoso'
-            from_email = 'vipermxm@gmail.com'
-            recipient_list = [request.POST["email"]]
-
-            # Utiliza la plantilla HTML para el correo electrónico
-            html_message = render_to_string('bienvenida.html', {"nombre": request.POST["nombre"]+' '+request.POST["apellidoPaterno"]+' '+request.POST["apellidoMaterno"]})
-            #{{nombre}} es como se llama la variable que mandamos
-            # Envía el correo electrónico
-            send_mail(subject, strip_tags(html_message), from_email, recipient_list, html_message=html_message)
-
-            return redirect('home')
-        
         except:
-            return render(request, self.template_name,{"error":""})
+            return render(request, self.template_name, {"error": ""})
 
 
 class home(APIView):
@@ -62,89 +85,103 @@ class home(APIView):
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class my_account(APIView):
-    template_name= "my_account.html"
+    template_name = "my_account.html"
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class about_us(APIView):
-    template_name= "about_us.html"
+    template_name = "about_us.html"
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class account_details(APIView):
-    template_name= "account_details.html"
+    template_name = "account_details.html"
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class addresses(APIView):
-    template_name= "addresses.html"
+    template_name = "addresses.html"
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class cart(APIView):
-    template_name= "cart.html"
+    template_name = "cart.html"
 
     def get(self, request):
-        return render(request, self.template_name)
+        usuarios = Usuario.objects.all()
+        return render(request, 'cart.html', {'usuarios': usuarios})
+        #return render(request, self.template_name)
     
+    #def vista_usuarios(request):
+     #   usuarios = Usuario.objects.all()
+      #  return render(request, 'cart.html', {'usuarios': usuarios})
+
+
 class order_list(APIView):
-    template_name= "order_list.html"
+    template_name = "order_list.html"
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class wishlist(APIView):
-    template_name= "wishlist.html"
+    template_name = "wishlist.html"
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class shop(APIView):
-    template_name= "shop.html"
+    template_name = "shop.html"
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class checkout_complate(APIView):
-    template_name= "checkout_complate.html"
+    template_name = "checkout_complate.html"
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class checkout_1(APIView):
-    template_name= "checkout_1.html"
+    template_name = "checkout_1.html"
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class checkout_2(APIView):
-    template_name= "checkout_2.html"
+    template_name = "checkout_2.html"
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class checkout_4(APIView):
-    template_name= "checkout_4.html"
+    template_name = "checkout_4.html"
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+
 class checkout_5(APIView):
-    template_name= "checkout_5.html"
+    template_name = "checkout_5.html"
 
     def get(self, request):
         return render(request, self.template_name)
-
-    
-
-
-
 
 
 
