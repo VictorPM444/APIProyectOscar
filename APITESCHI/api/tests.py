a3f551cc06c604a66d63ffe281594961da5d3e78
VictorPM444 <victorpmejia2701@gmail.com>
2023-11-21 19:49:05 -0600

    commit para cambio de carp

--- APITESCHI/api/tests.py
+++ APITESCHI/api/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+import pytest
+from api.models import Marca, Categoria, Material, Temporada, Genero, Talla, Color, Usuario
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_creacion_marca():
+    marca = Marca.objects.create(nombreMarca='Nike', descripcion='Zapatos deportivos')
+    assert marca.nombreMarca == 'Nike'
